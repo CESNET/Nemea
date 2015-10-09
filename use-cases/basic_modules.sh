1dfdbd9c1723dfe14598e706829d870a32a05614
Marek Svepes <svepemar@fit.cvut.cz>
2015-10-09 10:44:04 +0200

    use-cases: added new use-case (test of basic modules)
    - testing connection of all basic modules (nfreader, merger, logger, logreplay and flowcounter)
    - added netflow data file - random generated 6000 flows

---
+++ use-cases/basic_modules.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+# Author: Marek Svepes <svepemar@fit.cvut.cz>
+# Copyright (C) 2015 CESNET
+#
+# LICENSE TERMS
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materials provided with the
+#    distribution.
+# 3. Neither the name of the Company nor the names of its contributors
+#    may be used to endorse or promote products derived from this
+#    software without specific prior written permission.
+#
+# ALTERNATIVELY, provided that this notice is retained in full, this
+# product may be distributed under the terms of the GNU General Public
+# License (GPL) version 2 or later, in which case the provisions
+# of the GPL apply INSTEAD OF those given above.
+#
+# This software is provided ``as is'', and any express or implied
+# warranties, including, but not limited to, the implied warranties of
+# merchantability and fitness for a particular purpose are disclaimed.
+# In no event shall the company or contributors be liable for any
+# direct, indirect, incidental, special, exemplary, or consequential
+# damages (including, but not limited to, procurement of substitute
+# goods or services; loss of use, data, or profits; or business
+# interruption) however caused and on any theory of liability, whether
+# in contract, strict liability, or tort (including negligence or
+# otherwise) arising in any way out of the use of this software, even
+# if advised of the possibility of such damage.
+#
+
+
+# This script shows connection of the basic Nemea modules
+# (nfreader, merger, logger, logreplay and flowcounter). Three nfreader
+# modules read netflow data from input file and send them in unirec format
+# to merger module which sends incoming data from all input interfaces
+# to one output interface. The data are stored into csv file by logger module.
+# The csv file can be read by logreplay module which sends them again
+# in unirec format to flowcounter module. Result of this module is number
+# of received flows (it should be number of flows from nfreader input file x 3).
+
+#                     ----------
+#  netflow.data ---> | nfreader | ----------
+#                     ----------           |
+#                                          v
+#                     ----------        --------        --------
+#  netflow.data ---> | nfreader | ---> | merger | ---> | logger | ---> file.csv
+#                     ----------        --------        --------
+#                                          ^
+#                     ----------           |
+#  netflow.data ---> | nfreader | ----------
+#                     ----------
+
+#                 -----------        -------------
+#  file.csv ---> | logreplay | ---> | flowcounter | ---> Result (number of flows)
+#                 -----------        -------------
+
+# Input file for nfreader modules (netflow data)
+INPUT_FILE="./nfcap_6000_gener_flows.dat"
+# Logger output file (csv format)
+LOGGER_OUTPUT_FILE="./logger_test_out"
+
+printf ">>> Starting 3 nfreaders, merger and logger...\n>Nfreaders sent:\n"
+../modules/nfreader/nfdump_reader -i u:nfr1_test_out $INPUT_FILE &
+../modules/nfreader/nfdump_reader -i u:nfr2_test_out $INPUT_FILE &
+../modules/nfreader/nfdump_reader -i u:nfr3_test_out $INPUT_FILE &
+../modules/merger/merger -n 3 -i u:nfr1_test_out,u:nfr2_test_out,u:nfr3_test_out,u:merger_test_out &
+../modules/logger/logger -i u:merger_test_out -w $LOGGER_OUTPUT_FILE -t
+
+printf ">>> Starting logreplay and flowcounter...\n>Flowcounter received:\n"
+../modules/logreplay/logreplay -i u:logrep_test_out -f $LOGGER_OUTPUT_FILE &
+../modules/flowcounter/flowcounter -i u:logrep_test_out
+
+# Cleanup
+rm -f ./logger_test_out
