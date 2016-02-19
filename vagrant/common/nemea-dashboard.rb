c577cd8f33a437da6243fbaab9b482a29ce160a4
Tomas Cejka <cejkat@cesnet.cz>
2016-02-19 14:12:21 +0100

    vagrant: dashboard: store initial data (user)

--- vagrant/common/nemea-dashboard.rb
+++ vagrant/common/nemea-dashboard.rb
@@ -31,6 +31,32 @@ cd Nemea-Dashboard
 # install dashboard dependencies
 pip install -r requirements.txt
 
+mongo 127.0.0.1/nemeadb --eval '
+db.users.drop();
+var d = { "_id" : ObjectId("56b33e737194ed8d440db2b7"),
+"username" : "default",
+"password" : "$2a$12$Md5LyzT0ejjCIeQeUR3U5eLHrP602Vz7lDD6HiV5UWRITZythu6Tq",
+"settings" : [ { "row" : 0, "content" : "", "sizeX" : 3, "col" : 0,
+"title" : "Events ratios in 12 hours", "loading" : false, "type" : "piechart", "sizeY" : 3,
+"config" : { "begintime" : "2016-02-18T21:37:22.284Z", "metric" : "category", "period" : "12", "type" : "piechart" } },
+{ "row" : 2, "selector" : false, "timestamp" : "", "type" : "barchart", "sizeY" : 3,
+"config" : { "begintime" : "2016-02-18T21:37:22.305Z", "period" : "12", "metric" : "category", "type" : "barchart",
+"window" : "60" }, "sizeX" : 4, "col" : 3, "title" : "Last 24 hours in 1h window",
+"loading" : false, "minSizeX" : 2, "minSizeY" : 2 },
+{ "row" : 0, "sizeX" : 3, "sizeY" : 2, "title" : "Top events in 1 hour",
+"loading" : false, "type" : "top", "col" : 3, "config" : { "begintime" : "2016-02-19T08:37:22.312Z", "period" : 1 } },
+{ "row" : 1, "sizeX" : 1, "sizeY" : 1, "title" : "12h", "loading" : false, "type" : "sum", "col" : 6,
+"config" : { "begintime" : "2016-02-18T21:37:22.318Z", "period" : "12", "type" : "sum", "category" : "Attempt.Login" } },
+{ "row" : 0, "sizeX" : 1, "sizeY" : 1, "title" : "12h", "loading" : false, "type" : "sum", "col" : 6,
+"config" : { "begintime" : "2016-02-19T08:37:22.324Z", "period" : "1", "type" : "sum", "category" : "any" } },
+{ "row" : 0, "sizeX" : 1, "sizeY" : 1, "title" : "12h", "loading" : false, "type" : "sum", "col" : 7,
+"config" : { "begintime" : "2016-02-19T03:37:22.329Z", "period" : "6", "type" : "sum", "category" : "any" } },
+{ "row" : 1, "sizeX" : 1, "sizeY" : 1, "title" : "12h", "loading" : false, "type" : "sum", "col" : 7,
+"config" : { "begintime" : "2016-02-18T21:37:22.334Z", "period" : "12", "type" : "sum", "category" : "Availability.DoS" } } ]
+ };
+db.users.insert(d);
+'
+
 # start backend on background
 cd /var/log;
 nohup python3.4 /var/www/html/Nemea-Dashboard/apiv2.py > nemea-dashboard.out 2> nemea-dashboard.err < /dev/null &
