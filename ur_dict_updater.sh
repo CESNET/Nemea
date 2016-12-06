69c07f2ff1b0f5bd6d5037507f0b4e8ba51226f5
ladislavmacoun <ladislavmacoun@gmail.com>
2016-12-06 18:03:41 +0100

    Added conflict control .

--- ur_dict_updater.sh
+++ ur_dict_updater.sh
@@ -55,14 +55,16 @@ BEGIN {
    print "| ----- | ----- | ----- |"
 }
 /^..*$/ {
-
-# TODO conflict types control 
-
    if (name != $2) {
       type=$1
       name=$2
       desc=$3
-	
+
+	if ( (type != $1 ) && (name != $2) ){
+		printf("Conflicting types (%s, %s) of UniRec field (%s)\n", type, $1, iden);
+      exit 1;
+	}
+
       for (i=4; i<=NF; i++) {
          desc=desc" "$i
       }
